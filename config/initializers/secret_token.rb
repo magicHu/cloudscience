2ae5ff0bcc09228aacca1190e5844d9b64fe0c4e
MagicHu <huronghai2008@gmail.com>
2011-12-01 17:54:22 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudscience::Application.config.secret_token = '06808027eec79abea20efad6476c7ce760307729658974b05f1f723f1b9fa00a82e4559ed280af46e86c8e063bf33f34512fdf04191d4e48f43b857670508e82'
